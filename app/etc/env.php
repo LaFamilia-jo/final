3f14297a7184ab71695040e393cf7700ec0a5eec
Kamil Pathan <pkamil717@gmail.com>
2018-12-17 17:35:37 +0530

    modified env.php

--- app/etc/env.php
+++ app/etc/env.php
@@ -6,15 +6,35 @@ return [
   'crypt' => [
     'key' => '57a2596348a3596796b32621cc1efcbe'
   ],
-  'session' => [
-    'save' => 'files'
-  ],
+    'session' => [
+        'save' => 'redis',
+        'redis' => [
+            'host' => 'redis',
+            'port' => '6379',
+            'password' => '',
+            'timeout' => '2.5',
+            'persistent_identifier' => '',
+            'database' => '2',
+            'compression_threshold' => '2048',
+            'compression_library' => 'gzip',
+            'log_level' => '1',
+            'max_concurrency' => '6',
+            'break_after_frontend' => '5',
+            'break_after_adminhtml' => '30',
+            'first_lifetime' => '600',
+            'bot_first_lifetime' => '60',
+            'bot_lifetime' => '7200',
+            'disable_locking' => '0',
+            'min_lifetime' => '60',
+            'max_lifetime' => '2592000'
+        ]
+    ],
   'db' => [
     'table_prefix' => '',
     'connection' => [
       'default' => [
-        'host' => '180.211.112.84',
-        'dbname' => 'test',
+        'host' => 'db',
+        'dbname' => 'magento',
         'username' => 'admin',
         'password' => 'admin',
         'active' => '1'
@@ -43,6 +63,27 @@ return [
     'translate' => 1,
     'config_webservice' => 1
   ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'backend' => 'Cm_Cache_Backend_Redis',
+                'backend_options' => [
+                    'server' => 'redis',
+                    'database' => '0',
+                    'port' => '6379'
+                ]
+            ],
+            'page_cache' => [
+                'backend' => 'Cm_Cache_Backend_Redis',
+                'backend_options' => [
+                    'server' => 'redis',
+                    'port' => '6379',
+                    'database' => '1',
+                    'compress_data' => '0'
+                ]
+            ]
+        ]
+    ],
   'install' => [
     'date' => 'Tue, 11 Dec 2018 07:44:53 +0000'
   ]

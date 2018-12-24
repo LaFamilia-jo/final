a82c5bf2d697f1d43722b6e80cba5dd4ca166ade
Kamil Pathan <pkamil717@gmail.com>
2018-12-24 18:20:28 +0530

    Dockerfile and docker-entry-point.sh

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 #set -e
 # Recommend syntax for setting an infinite while loop
 #service php7.2-fpm start
-service nginx start
+#service nginx start
+echo "123"
 #Extra line added in the script to run all command line arguments
 #exec "$@";

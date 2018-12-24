df1bc14ff372ec77706307a4d9eb9ebb83c6f6b5
Kamil Pathan <pkamil717@gmail.com>
2018-12-24 18:34:33 +0530

    Dockerfile and docker-entry-point.sh

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -3,6 +3,16 @@
 # Recommend syntax for setting an infinite while loop
 #service php7.2-fpm start
 #service nginx start
-echo "123"
+
+set -e
+# Recommend syntax for setting an infinite while loop
+while :
+do
+	echo "Do something; hit [CTRL+C] to stop!"
+done
+
+
+#Extra line added in the script to run all command line arguments
+exec "$@";
 #Extra line added in the script to run all command line arguments
 #exec "$@";
